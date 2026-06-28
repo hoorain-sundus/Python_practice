9778a04d72a1c6c5e90cc57eacf1e5ae94a83632
Hoorain Sundus <hoorain.sunduss@gmail.com>
2026-06-28 20:46:47 +0800

    Input

--- practice.py
+++ practice.py
@@ -5,3 +5,11 @@ b = str(a) # a but type should be string
 
 t = type(b)
 print(t)
+
+#------------------------ Input -------------------------------------
+# Use int function because it takes numbers in form of string and concatinate it.
+
+c = int(input("Enter number 1: "))
+d = int(input("Enter number 2: "))
+
+print(c + d)
