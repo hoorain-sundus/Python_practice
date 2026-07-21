c42737ee8c23f7142b8c54e5e1797597a2e38964
Hoorain Sundus <hoorain.sunduss@gmail.com>
2026-07-21 23:10:56 +0500

    Loops

--- practice.py
+++ practice.py
@@ -212,3 +212,21 @@ for i in range(100):
 
 for i in range(100):
     pass;
+
+
+# -------------------------- LOOPS -------------------------------------
+
+'''
+for n = 3
+
+  *
+ ***
+***** 
+'''
+
+
+n = int(input("Enter the number: "))
+for i in range(1, n+1):
+    print(" "* (n-i), end="")
+    print("*"* (2*i-1), end="")
+    print("")
